9fd7c9417ddb8e470ffdce8206b062da8e280ddf
Alec Von Arx <al.vonarx+fridge-dev@gmail.com>
2021-01-08 23:07:40 -0800

    Change InMemoryLog to exercise conversion logic

--- src/commitlog/in_memory.rs
+++ src/commitlog/in_memory.rs
@@ -1,29 +1,41 @@
 use crate::commitlog::{Entry, Index, Log};
 use std::io;
+use std::marker::PhantomData;
 
 // I am focusing on learning raft's consensus algorithm, not strictly about exactly how to
 // persist the data durably. I will theoretically model it here.
 //
 // To improve on this, do something similar to: https://thehoard.blog/how-kafkas-storage-internals-work-3a29b02e026
 pub struct InMemoryLog<E: Entry> {
-    log: Vec<E>,
+    // We don't *need* to convert these to bytes. We could just hold the original entry in memory,
+    // but we want to exercise the conversion logic.
+    log: Vec<Vec<u8>>,
+    _pd: PhantomData<E>,
 }
 
 impl<E: Entry> InMemoryLog<E> {
     pub fn new() -> Self {
-        InMemoryLog { log: vec![] }
+        InMemoryLog {
+            log: vec![],
+            _pd: PhantomData::default(),
+        }
     }
 }
 
 impl<E: Entry> Log<E> for InMemoryLog<E> {
     fn append(&mut self, entry: E) -> Result<Index, io::Error> {
-        self.log.push(entry);
+        self.log.push(entry.into());
 
         Ok(Index::from(self.log.len() - 1))
     }
 
     fn read(&self, index: Index) -> Result<Option<E>, io::Error> {
-        Ok(self.log.get(index.val() as usize).cloned())
+        let opt_entry = self.log
+            .get(index.val() as usize)
+            .cloned()
+            .map(|b| E::from(b));
+
+        Ok(opt_entry)
     }
 
     fn truncate(&mut self, index: Index) {
