2f2ab02006356e5e33295ab9fcf1c14586caafaa
Alec Von Arx <al.vonarx+fridge-dev@gmail.com>
2021-02-04 22:24:40 -0800

    Fix typo and add note about module structure

--- src/lib.rs
+++ src/lib.rs
@@ -18,9 +18,10 @@ pub use api::WriteToLogError;
 pub use api::WriteToLogInput;
 pub use api::WriteToLogOutput;
 
-// Learning 1: `create::{root_mod}` should not have any code. Just `mod` and `pub use` statements.
+// Learning 1: `crate::{root_mod}` should not have any code. Just `mod` and `pub use` statements.
 // Learning 2: All `mod` statements, anywhere, should not be `pub`. Only export `pub` via individual
 //             use statements.
 //
 // This keeps the `crate::{root_mod}` root_mod only responsible for exporting types to the rest of
-// crate, and allows me to organize my root_mod impl however I want.
+// crate, and allows me to organize my root_mod impl however I want. This is because I really like
+// go's style of packaging/visibility and I'm going for something similar here.
