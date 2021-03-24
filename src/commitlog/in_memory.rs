2f713bf40394b62d2771f8cc77b1ec15b7f945bf
Alec Von Arx <al.vonarx+fridge-dev@gmail.com>
2021-03-24 00:21:05 -0700

    Add artificial sleep when interacting with InMemoryLog to simulate latency of accessing disk

--- src/commitlog/in_memory.rs
+++ src/commitlog/in_memory.rs
@@ -1,6 +1,7 @@
 use crate::commitlog::{Entry, Index, Log};
-use std::io;
+use std::{io, thread};
 use std::marker::PhantomData;
+use std::time::Duration;
 
 // I am focusing on learning raft's consensus algorithm, not strictly about exactly how to
 // persist the data durably. I will theoretically model it here.
@@ -34,6 +35,7 @@ impl<E: Entry> Log<E> for InMemoryLog<E> {
     fn append(&mut self, entry: E) -> Result<Index, io::Error> {
         let entry = entry.into();
         slog::info!(self.info_log, "{:?}", entry);
+        LogAction::Write.simulate_disk_latency();
         self.log.push(entry.into());
 
         Ok(Index::new_usize(self.log.len()))
@@ -41,6 +43,7 @@ impl<E: Entry> Log<E> for InMemoryLog<E> {
 
     fn read(&self, index: Index) -> Result<Option<E>, io::Error> {
         let vec_index = Self::vec_index(index);
+        LogAction::Read.simulate_disk_latency();
         let opt_entry = self.log.get(vec_index).cloned().map(|b| E::from(b));
 
         Ok(opt_entry)
@@ -48,10 +51,26 @@ impl<E: Entry> Log<E> for InMemoryLog<E> {
 
     fn truncate(&mut self, index: Index) {
         let vec_index = Self::vec_index(index);
+        LogAction::Write.simulate_disk_latency();
         self.log.truncate(vec_index)
     }
 
     fn next_index(&self) -> Index {
         Index::new_usize(self.log.len() + 1)
+    }
+}
+
+enum LogAction {
+    Read,
+    Write,
+}
+
+impl LogAction {
+    fn simulate_disk_latency(&self) {
+        let latency_ms = match self {
+            LogAction::Read => 1,
+            LogAction::Write => 2,
+        };
+        thread::sleep(Duration::from_millis(latency_ms));
     }
 }
