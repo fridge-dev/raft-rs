d3a2fbd0b63cde6bbc25e423f49b6e22c9a70c1c
Alec Von Arx <al.vonarx+fridge-dev@gmail.com>
2021-05-08 01:13:21 -0700

    Change follower timer task to use Clock abstraction and write tests with mock clock

--- src/replica/timers.rs
+++ src/replica/timers.rs
@@ -106,11 +106,12 @@ impl<C: Clock + Send + Sync + 'static> LeaderTimerHandle<C> {
     }
 }
 
-pub struct FollowerTimerHandle {
+pub struct FollowerTimerHandle<C: Clock = RealClock> {
     // We use flume instead of tokio because we need non-blocking try_recv() and tokio's was removed in 1.0
     // TODO:3 optimize to use `SharedOption` but would need to account for the 3 try_recv branches.
     wake_time_queue: flume::Sender<Instant>,
     timeout_range: RangeInclusive<Duration>,
+    clock: C,
 }
 
 impl FollowerTimerHandle {
@@ -119,15 +120,28 @@ impl FollowerTimerHandle {
         max_timeout: Duration,
         actor_client: actor::ActorClient,
     ) -> Self {
+        Self::spawn_background_task_with_clock(min_timeout, max_timeout, actor_client, RealClock)
+    }
+}
+
+impl<C: Clock + Send + Sync + 'static> FollowerTimerHandle<C> {
+    // For tests
+    fn spawn_background_task_with_clock(
+        min_timeout: Duration,
+        max_timeout: Duration,
+        actor_client: actor::ActorClient,
+        clock: C,
+    ) -> Self {
         let (tx, rx) = flume::unbounded();
 
         let handle = FollowerTimerHandle {
             wake_time_queue: tx,
             timeout_range: RangeInclusive::new(min_timeout, max_timeout),
+            clock: clock.clone(),
         };
         handle.reset_timeout();
 
-        tokio::task::spawn(Self::follower_timer_task(rx, actor_client));
+        tokio::task::spawn(Self::follower_timer_task(rx, actor_client, clock));
 
         handle
     }
@@ -146,15 +160,15 @@ impl FollowerTimerHandle {
 
     fn random_wake_time(&self) -> Instant {
         let rand_timeout = rand::thread_rng().gen_range(self.timeout_range.clone());
-        Instant::now() + rand_timeout
+        self.clock.now() + rand_timeout
     }
 
-    async fn follower_timer_task(queue: flume::Receiver<Instant>, actor_client: actor::ActorClient) {
+    async fn follower_timer_task(queue: flume::Receiver<Instant>, actor_client: actor::ActorClient, mut clock: C) {
         loop {
             match queue.try_recv() {
                 Ok(wake_time) => {
                     // We've received a leader heartbeat, so we sleep until the next timeout.
-                    tokio::time::sleep_until(wake_time).await;
+                    clock.sleep_until(wake_time).await;
                 }
                 Err(flume::TryRecvError::Empty) => {
                     // We slept until `wake_time` and didn't receive another message. If the queue
@@ -197,6 +211,7 @@ impl<T> SharedOption<T> {
     }
 }
 
+// TODO:1 move this into some base level crate and export mocks as a test-util feature flag.
 mod time {
     use tokio::sync::watch;
     use tokio::time::{Duration, Instant};
@@ -269,7 +284,7 @@ mod time {
             self.current_time() - self.time_of_instantiation
         }
 
-        /// Advancing by large steps of time can cause unexpected state in `sleep_until()` usage.
+        /// Advancing by large steps of time can cause surprising behavior in `sleep_until()` usage.
         /// The only promise of mock `sleep_until` is that it will return when `now` is at or past
         /// the `deadline`. For example, if you call...
         ///
@@ -381,8 +396,8 @@ mod stop_signal {
 #[cfg(test)]
 mod tests {
     use crate::actor::{ActorClient, Event};
-    use crate::replica::timers::time;
     use crate::replica::timers::LeaderTimerHandle;
+    use crate::replica::timers::{time, FollowerTimerHandle};
     use crate::replica::{LeaderTimerTick, ReplicaId, Term};
     use std::fmt::Debug;
     use tokio::sync::mpsc;
@@ -401,7 +416,7 @@ mod tests {
             self.rx.recv().await.expect("Expected value")
         }
 
-        pub async fn recv_assert_closed(&mut self) {
+        pub async fn recv_assert_empty_and_closed(&mut self) {
             if let Some(_) = self.rx.recv().await {
                 panic!("Expected None");
             }
@@ -417,21 +432,27 @@ mod tests {
     struct TestUtilActor {
         receiver: TestUtilReceiver<Event>,
         timeout: Duration,
-        expected_leader_heartbeat: LeaderTimerTick,
     }
 
     impl TestUtilActor {
-        pub fn new(actor_queue_rx: mpsc::Receiver<Event>, peer_id: ReplicaId, term: Term) -> Self {
+        pub fn new(actor_queue_rx: mpsc::Receiver<Event>) -> Self {
             TestUtilActor {
                 receiver: TestUtilReceiver::new(actor_queue_rx),
                 timeout: Duration::from_millis(10),
-                expected_leader_heartbeat: LeaderTimerTick { peer_id, term },
             }
         }
 
-        pub async fn assert_leader_heartbeat_event(&mut self) {
+        pub async fn assert_leader_heartbeat_event(&mut self, expected_leader_heartbeat: LeaderTimerTick) {
             if let Event::LeaderTimer(event) = self.receiver.recv().await {
-                assert_eq!(event, self.expected_leader_heartbeat);
+                assert_eq!(event, expected_leader_heartbeat);
+            } else {
+                panic!("Unexpected event");
+            }
+        }
+
+        pub async fn assert_follower_timeout_event(&mut self) {
+            if let Event::FollowerTimeout = self.receiver.recv().await {
+                // Success!
             } else {
                 panic!("Unexpected event");
             }
@@ -448,9 +469,14 @@ mod tests {
         let heartbeat_timeout = Duration::from_millis(100);
         let (tx, rx) = mpsc::channel(10);
         let actor_client = ActorClient::new(tx);
+        let mut actor = TestUtilActor::new(rx);
+
         let peer_id = ReplicaId::new("peer-123");
         let term = Term::new(10);
-        let mut actor_queue = TestUtilActor::new(rx, peer_id.clone(), term);
+        let expected_heartbeat_event = LeaderTimerTick {
+            peer_id: peer_id.clone(),
+            term,
+        };
 
         let (mock_clock, mut mock_clock_controller) = time::mocked_clock();
 
@@ -465,25 +491,31 @@ mod tests {
             mock_clock,
         );
 
-        actor_queue.assert_leader_heartbeat_event().await;
-        actor_queue.assert_no_event().await;
+        actor
+            .assert_leader_heartbeat_event(expected_heartbeat_event.clone())
+            .await;
+        actor.assert_no_event().await;
 
         // 2. Advance time and receive heartbeat many times
         for _ in 0..5 {
             mock_clock_controller.advance(heartbeat_timeout);
-            actor_queue.assert_leader_heartbeat_event().await;
-            actor_queue.assert_no_event().await;
+            actor
+                .assert_leader_heartbeat_event(expected_heartbeat_event.clone())
+                .await;
+            actor.assert_no_event().await;
         }
 
         // 3. Advance time by a big leap, still receive single heartbeat
         mock_clock_controller.advance(heartbeat_timeout * 5);
-        actor_queue.assert_leader_heartbeat_event().await;
-        actor_queue.assert_no_event().await;
+        actor
+            .assert_leader_heartbeat_event(expected_heartbeat_event.clone())
+            .await;
+        actor.assert_no_event().await;
 
         // 4. Drop handle and assert closed
         drop(timer_handle);
         mock_clock_controller.advance(heartbeat_timeout);
-        actor_queue.receiver.recv_assert_closed().await;
+        actor.receiver.recv_assert_empty_and_closed().await;
     }
 
     #[tokio::test]
@@ -492,9 +524,14 @@ mod tests {
         let heartbeat_timeout = Duration::from_millis(100);
         let (tx, rx) = mpsc::channel(10);
         let actor_client = ActorClient::new(tx);
+        let mut actor = TestUtilActor::new(rx);
+
         let peer_id = ReplicaId::new("peer-123");
         let term = Term::new(10);
-        let mut actor_queue = TestUtilActor::new(rx, peer_id.clone(), term);
+        let expected_heartbeat_event = LeaderTimerTick {
+            peer_id: peer_id.clone(),
+            term,
+        };
 
         let (mock_clock, mut mock_clock_controller) = time::mocked_clock();
 
@@ -509,8 +546,10 @@ mod tests {
             mock_clock,
         );
 
-        actor_queue.assert_leader_heartbeat_event().await;
-        actor_queue.assert_no_event().await;
+        actor
+            .assert_leader_heartbeat_event(expected_heartbeat_event.clone())
+            .await;
+        actor.assert_no_event().await;
 
         // 2a. Repeatedly advance time by 0.5 and reset heartbeat timer
         for _ in 0..5 {
@@ -518,7 +557,7 @@ mod tests {
             timer_handle.reset_heartbeat_timer();
         }
         // 2b. Assert no heartbeat (because we reset it!).
-        actor_queue.assert_no_event().await;
+        actor.assert_no_event().await;
 
         // Sanity check that T=2.5.
         // Notice heartbeat timeout should be set for T=3.5.
@@ -526,15 +565,129 @@ mod tests {
 
         // 3a. Advance time to T=3, assert no heartbeat (because we reset it!)
         mock_clock_controller.advance(heartbeat_timeout / 2);
-        actor_queue.assert_no_event().await;
+        actor.assert_no_event().await;
 
         // 3b. Advance time to T=3.5, assert heartbeat (timeout occurs)
         mock_clock_controller.advance(heartbeat_timeout / 2);
-        actor_queue.assert_leader_heartbeat_event().await;
+        actor
+            .assert_leader_heartbeat_event(expected_heartbeat_event.clone())
+            .await;
 
         // Sanity check that T=3.5
         assert_eq!(mock_clock_controller.elapsed_time(), heartbeat_timeout * 7 / 2);
     }
 
-    // TODO:1.5 follower timer tests
+    #[tokio::test]
+    async fn follower_timer_handle_reset_and_timeout() {
+        // -- setup --
+        let timeout = Duration::from_millis(100);
+        let (tx, rx) = mpsc::channel(10);
+        let actor_client = ActorClient::new(tx);
+        let mut actor = TestUtilActor::new(rx);
+
+        let (mock_clock, mut mock_clock_controller) = time::mocked_clock();
+
+        // -- execute & verify --
+
+        // 1. Spawn task, assert there is no event in the queue.
+        let timer_handle = FollowerTimerHandle::spawn_background_task_with_clock(
+            // We are not testing randomness/jitter, so make min/max the same.
+            /* min */ timeout,
+            /* max */ timeout,
+            actor_client,
+            mock_clock,
+        );
+
+        actor.assert_no_event().await;
+
+        // 2. Advance time and reset timeout many times, assert no event
+        for _ in 0..5 {
+            mock_clock_controller.advance(timeout / 2);
+            timer_handle.reset_timeout();
+        }
+        actor.assert_no_event().await;
+
+        // Sanity check T=2.5
+        assert_eq!(mock_clock_controller.elapsed_time(), timeout * 5 / 2);
+
+        // 3. Validate no timeout occurs at T < 3.5
+        let one_ns = Duration::from_nanos(1);
+        mock_clock_controller.advance(timeout - one_ns);
+        actor.assert_no_event().await;
+
+        // 4. Validate timeout occurs and timer task exits at exactly T >= 3.5, because our last
+        // timeout reset was at T=2.5.
+        mock_clock_controller.advance(one_ns);
+        actor.assert_follower_timeout_event().await;
+        actor.receiver.recv_assert_empty_and_closed().await;
+
+        // 5. Drop timer and validate no panics or anything.
+        drop(timer_handle);
+    }
+
+    #[tokio::test]
+    async fn follower_timer_handle_drop() {
+        // -- setup --
+        let timeout = Duration::from_millis(100);
+        let (tx, rx) = mpsc::channel(10);
+        let actor_client = ActorClient::new(tx);
+        let mut actor = TestUtilActor::new(rx);
+
+        let (mock_clock, mut mock_clock_controller) = time::mocked_clock();
+
+        // -- execute --
+        // Spawn task, it will be sleeping. Drop handle to observe behavior.
+        let timer_handle = FollowerTimerHandle::spawn_background_task_with_clock(
+            // We are not testing randomness/jitter, so make min/max the same.
+            /* min */ timeout,
+            /* max */ timeout,
+            actor_client,
+            mock_clock,
+        );
+        drop(timer_handle);
+
+        // -- verify --
+        // Fast-fwd time to ensure timer task would've fired an event, and then assert timer task
+        // has exited.
+        mock_clock_controller.advance(timeout * 2);
+        actor.receiver.recv_assert_empty_and_closed().await;
+    }
+
+    // TODO:1 refactor Follower timer task to fix this bug. Current thought: Change it to be an
+    //        async queue with a literal timeout on the receiver side lmao duh.
+    /// TDD: Fixes bug of previous impl. That's why this test looks oddly specific and minimal.
+    #[tokio::test]
+    async fn follower_timer_handle_reset_timeout_after_timer_task_exit() {
+        // -- setup --
+        let timeout = Duration::from_millis(100);
+        let (tx, rx) = mpsc::channel(10);
+        let actor_client = ActorClient::new(tx);
+        let mut actor = TestUtilActor::new(rx);
+
+        let (mock_clock, mut mock_clock_controller) = time::mocked_clock();
+
+        // Spawn task, assert there is no event in the queue.
+        let timer_handle = FollowerTimerHandle::spawn_background_task_with_clock(
+            // We are not testing randomness/jitter, so make min/max the same.
+            /* min */ timeout,
+            /* max */ timeout,
+            actor_client,
+            mock_clock,
+        );
+        actor.assert_no_event().await;
+
+        // -- execute --
+        // Trigger timeout (causes timer task to exit), attempt to reset timeout. This might appear
+        // as though the ordering is impossible for the single-threaded actor to observe. But it is
+        // possible for timer task to notify actor queue of a timeout while there is already an
+        // AppendEntries (i.e. reset_timeout) message is in the actor queue. We wait for actor to
+        // the receive timeout first here because that's the only point of synchronization we have
+        // available to ensure the task has triggered a timeout.
+        mock_clock_controller.advance(timeout);
+        actor.assert_follower_timeout_event().await;
+        timer_handle.reset_timeout();
+
+        // -- verify --
+        // Assert no panic!
+    }
 }
